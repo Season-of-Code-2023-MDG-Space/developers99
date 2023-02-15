909f277a44c3d785b010185c39f2d8d09b995519
rudy <96530069+rudransh0210@users.noreply.github.com>
2023-02-16 03:56:57 +0530

    Added Graph functionality(not fully functionable) and started scraping the data from api.

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: BottomSelectionWidget(),
+    );
+  }
+}
+//API KEY = VTHIJHII6A4M4MSF
+String stringResponse = "initial";
+Map mapResponse = Map();
+Map mapResponse1 = Map();
+List listRespone = [];
+List listDateswithtime = [];
+class BottomSelectionWidget extends StatefulWidget {
+  const BottomSelectionWidget({super.key});
+
+  @override
+  State<BottomSelectionWidget> createState() => _BottomSelectionWidgetState();
+}
+
+class _BottomSelectionWidgetState extends State<BottomSelectionWidget> {
+  Future apicall() async {
+    http.Response response;
+    response = await http.get(
+        Uri.parse("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=1min&apikey=demo"));
+    if(response.statusCode == 200)
+      {
+        setState(() {
+          mapResponse = json.decode(response.body);
+          mapResponse1 = mapResponse["Time Series (1min)"];
+          listDateswithtime = mapResponse1.keys.toList();
+        });
+      }
+    else
+      {
+        setState(() {
+          listRespone = ['error'];
+        });
+      }
+  }
+  late List<ChartData> _chartData;
+  @override
+  void initState() {
+    _chartData = getChartData();
+    apicall();
+    super.initState();
+  }
+
+  int i = 0;
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(listDateswithtime[0].substring(0, 10)),
+      ) ,
+        body:
+        Column(
+          children: [
+            SfCartesianChart(
+              series: <CandleSeries>[
+                CandleSeries<ChartData, DateTime>
+                  (dataSource: _chartData,
+                    xValueMapper: (ChartData sales, _) => sales.x,
+                    lowValueMapper: (ChartData sales, _) => sales.low,
+                    highValueMapper: (ChartData sales, _) => sales.high,
+                    openValueMapper: (ChartData sales, _) => sales.open,
+                    closeValueMapper: (ChartData sales, _) => sales.close)
+              ],
+              primaryXAxis: DateTimeAxis(),
+            ),
+          Row(
+          children: [Padding(padding: EdgeInsets.only(left: 10, top: 2),
+          child: Text('Time' + "               " + "Value", style: TextStyle(fontSize: 20, color: Colors.red)))]
+        ),
+          Expanded(child: Container(
+            padding: EdgeInsets.only(left: 2),
+          child: ListView.builder(padding: const EdgeInsets.only(top: 5),
+          itemCount: listDateswithtime.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              child: Container(child: Text(listDateswithtime[index].substring(10) + "                 " + mapResponse1[listDateswithtime[index]]["1. open"],
+                  style: TextStyle(fontSize: 15))),
+                );
+              }
+            )
+          )
+          )
+        ]
+       )
+    );
+  }
+  List<ChartData> getChartData()
+  {
+    return <ChartData>[
+      for(int i = 0; i < listDateswithtime.length; i++)
+        ChartData(x: DateTime(2023, 2, 24, listDateswithtime[i].substring(10, 13), listDateswithtime[i].substring(14, 16)),
+        open: mapResponse1[listDateswithtime[i]]["1. open"],
+        close: mapResponse1[listDateswithtime[i]]["4. close"],
+        low: mapResponse1[listDateswithtime[i]]["3. low"],
+        high: mapResponse1[listDateswithtime[i]]["2. high"],
+        )
+    ];
+  }
+}
+
+class ChartData{
+  ChartData({this.x,
+  this.open,
+  this.close,
+  this.high,
+  this.low});
+
+  final DateTime? x;
+  final num? open;
+  final num? close;
+  final num? low;
+  final num? high;
+}
+
+
